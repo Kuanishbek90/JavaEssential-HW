959ffec63db9c8ed2be189ce172d79525fddff21
Quanishbek <kuanysbekismanov@gmail.com>
2022-02-06 10:25:56 +0500

    MASTER 2: homework2

--- src/Lesson1/Task1.java
+++ src/Lesson1/Task1.java
@@ -3,5 +3,4 @@ package Lesson1;
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Hello World  !");
-    }
-}
+    }}
--- src/Lesson1/Task2.java
+++ src/Lesson1/Task2.java
@@ -2,6 +2,7 @@ package Lesson1;
 
 public class Task2 {
     public static void main(String[] args) {
-        System.out.println("1242346");
+        System.out.println("control");
+        System.out.println("1234");
     }
 }
